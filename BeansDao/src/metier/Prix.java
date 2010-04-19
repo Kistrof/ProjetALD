3cbf321d78a067dc60d4261128c64096fddaa5c0
Kistrof <chris.clemence@hotmail.fr>
2010-04-19 17:59:10 +0200

    Remplissage des beans Ceremonie, Prix et Pro

--- BeansDao/src/metier/Prix.java
+++ BeansDao/src/metier/Prix.java
@@ -1,5 +1,52 @@
 package metier;
 
-public class Prix {
-	// TODO
+public class Prix
+{
+	private int id;
+	private String titre;
+	
+	public Prix()
+	{
+		this.id = -1;
+		this.titre = "";
+	}
+	
+	public Prix(int id, String titre)
+	{
+		this.id = id;
+		this.titre = titre;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getTitre() {
+		return titre;
+	}
+	
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	
+	public boolean equals(Object obj)
+	{
+		if (obj instanceof Prix)
+		{
+			Prix p = (Prix)obj;
+			return p.getId() == this.getId();
+		}
+		else
+			return false;
+	}
+	
+	public String toString()
+	{
+		return "Prix["+this.getTitre()+"]";
+	}
+	
 }
--- BeansDao/src/metier/Pro.java
+++ BeansDao/src/metier/Pro.java
@@ -1,5 +1,76 @@
 package metier;
 
-public class Pro {
-	// TODO
+import java.util.Date;
+
+public class Pro
+{
+	private String pseudo;
+	private String pass;
+	private Date date_inscription;
+	private Date derniere_visite;
+	
+	public Pro()
+	{
+		this.pseudo = "";
+		this.pass = "";
+		date_inscription = new Date();
+		derniere_visite = new Date();
+	}
+
+	public Pro(String pseudo, String pass, Date dateInscription, Date derniereVisite)
+	{
+		this.pseudo = pseudo;
+		this.pass = pass;
+		date_inscription = dateInscription;
+		derniere_visite = derniereVisite;
+	}
+
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public Date getDate_inscription() {
+		return date_inscription;
+	}
+
+	public void setDate_inscription(Date dateInscription) {
+		date_inscription = dateInscription;
+	}
+
+	public Date getDerniere_visite() {
+		return derniere_visite;
+	}
+
+	public void setDerniere_visite(Date derniereVisite) {
+		derniere_visite = derniereVisite;
+	}
+	
+	public boolean equals(Object obj)
+	{
+		if (obj instanceof Pro)
+		{
+			Pro p = (Pro)obj;
+			return p.getPseudo().equals(this.getPseudo());
+		}
+		else
+			return false;
+	}
+	
+	public String toString()
+	{
+		return "Pro["+this.getPseudo()+"]";
+	}
+
 }
