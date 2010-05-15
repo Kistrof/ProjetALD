2dc54a0d2283924ed1f7eecccc549ce2c9ea4681
Kistrof <chris.clemence@hotmail.fr>
2010-05-15 15:55:54 +0200

    L'action AfficheFilm.do fonctionne de bout en bout, il manque juste une "vraie" vue

---
+++ Web/WebContent/vues/AfficheFilm.jsp
@@ -0,0 +1,17 @@
+<%@ taglib prefix="html" uri="http://struts.apache.org/tags-html" %>
+<%@ taglib prefix="bean" uri="http://struts.apache.org/tags-bean" %>
+<%@ taglib prefix="logic" uri="http://struts.apache.org/tags-logic" %>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Fiche Film</title>
+</head>
+<body>
+
+<bean:write name="FILM" property="titre" />
+
+</body>
+</html>
--- Web/src/struts/action/ActionAfficheFilm.java
+++ Web/src/struts/action/ActionAfficheFilm.java
@@ -9,6 +9,8 @@ import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
+import org.apache.struts.action.ActionMessages;
 
 import dao.DAOFilm;
 
@@ -22,10 +24,28 @@ public class ActionAfficheFilm extends Action
 
 	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
 	{
-		String id_str = request.getParameter("id");
-		int id = Integer.parseInt(id_str);
-		Film f = daoFilm.get(id);
-		request.setAttribute("FILM", f);
-		return mapping.findForward("vueAfficheFilm");
+		try
+		{
+			String id_str = request.getParameter("id");
+			int id = Integer.parseInt(id_str);
+			Film f = daoFilm.get(id);
+			if (f == null) throw new NullPointerException();
+			request.setAttribute("FILM", f);
+			return mapping.findForward("vueAfficheFilm");
+		}
+		catch (NumberFormatException ex)
+		{
+			ActionMessages erreurs = this.getErrors(request);
+			erreurs.add("Personne introuvable", new ActionMessage("erreurs.film.code_incorrect"));
+			this.addErrors(request, erreurs);
+			return mapping.getInputForward();
+		}
+		catch (NullPointerException ex)
+		{
+			ActionMessages erreurs = this.getErrors(request);
+			erreurs.add("Personne introuvable", new ActionMessage("erreurs.film.not_found"));
+			this.addErrors(request, erreurs);
+			return mapping.getInputForward();
+		}
 	}
 }
