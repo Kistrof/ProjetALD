08bcfc965e344518b5405744ff4db3683ae8e37d
Benoit <benoit@.(none)>
2010-05-19 01:02:29 +0200

    Ajout de la fonction loadAll(String chaine) qui recupere tous les film ayant un titre contenant le parametre chaine

--- Web/src/dao/DAOFilm.java
+++ Web/src/dao/DAOFilm.java
@@ -8,6 +8,7 @@ public interface DAOFilm
 {
 	public Film get(int id);
 	public ArrayList<Film> loadAll();
+	public ArrayList<Film> loadAll(String chaine);
 	public void save(Film obj);
 	public void delete(Film obj);
 	public void update(Film obj);
