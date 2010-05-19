70e728c4e426c361de3b9a4f49b191d569a49e42
Kistrof <chris.clemence@hotmail.fr>
2010-05-19 19:27:50 +0200

    Correction de la méthode get (gestion de l'id composé) Ajout d'une méthode qui calcule la moyenne d'un film

--- Web/src/dao/hibernate/DAONoteHbn.java
+++ Web/src/dao/hibernate/DAONoteHbn.java
@@ -6,15 +6,23 @@ import org.hibernate.Session;
 
 import dao.DAONote;
 
+import metier.Film;
 import metier.Note;
-
-
 
 public class DAONoteHbn extends DAOHibernate implements DAONote
 {
 
 	public DAONoteHbn()
 	{
+	}
+	
+	public Note get(Note note)
+	{
+		Note n = null;
+		Session s = this.connect();
+		n = (Note)s.get(Note.class, note);
+		this.close(s);
+		return n;
 	}
 
 	@Override
@@ -24,46 +32,48 @@ public class DAONoteHbn extends DAOHibernate implements DAONote
 		s.delete(obj);
 		this.close(s);
 	}
-
-	@Override
-	public Note get(int id)
-	{
-		Note f = null;
-		Session s = this.connect();
-		f = (Note) s.get(Note.class, id);
-		this.close(s);
-		return f;
-	}
-
+	
 	@Override
 	@SuppressWarnings("unchecked")
 	public ArrayList<Note> loadAll()
 	{
-		ArrayList<Note>tab = null;
+		ArrayList<Note> tab = null;
 		Session s = this.connect();
 		tab = (ArrayList<Note>) s.createQuery("FROM Note").list();
 		this.close(s);
 		return tab;
 	}
-
+	
+	@Override
+	@SuppressWarnings("unchecked")
+	public double getMoyenne(Film f)
+	{
+		double moy = 0;
+		Session s = this.connect();
+		ArrayList<Note> tab = (ArrayList<Note>) s.createQuery("FROM Note WHERE film = "+f.getId()).list();
+		for (int i = 0 ; i < tab.size() ; i++)
+			moy += tab.get(i).getValeur();
+		moy /= tab.size();
+		this.close(s);
+		return moy;
+	}
+	
 	@Override
 	public void save(Note obj)
 	{
-
-			Session s = this.connect();
+		Session s = this.connect();
+		Note n = this.get(obj);
+		if (n == null)
 			s.save(obj);
-			this.close(s);
-		
+		this.close(s);
 	}
-
+	
 	@Override
 	public void update(Note obj)
 	{
-
-			Session s = this.connect();
-			s.update(obj);
-			this.close(s);
-		
+		Session s = this.connect();
+		s.update(obj);
+		this.close(s);
 	}
 
 }
